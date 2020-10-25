1e0a3341f68a02fd81a7dd0fdc38a2f85fc344e9
Joshua <joshua@routeique.com>
2020-10-25 13:07:49 -0600

    Fixed bug with date validation

--- src/components/Dashboard/QuickQuoteForm.tsx
+++ src/components/Dashboard/QuickQuoteForm.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {TransportationType} from "../../utils/enums";
 import {
   Button,
@@ -7,13 +7,12 @@ import {
   Grid,
   InputLabel,
   MenuItem,
-  Select, Snackbar,
+  Select,
   TextField,
 } from '@material-ui/core';
 import {Field, FieldProps, Form, Formik, FormikProps} from "formik";
 import {InitialQuoteFormValues} from '../../utils/interfaces';
-import {Alert} from '@material-ui/lab';
-import {createQuoteSchema} from "../../validation/createQuoteSchema";
+import createQuoteSchema from "../../validation/createQuoteSchema";
 
 interface QuickQuoteProps {
   initialValues: InitialQuoteFormValues;
@@ -21,209 +20,167 @@ interface QuickQuoteProps {
   formSubmitted: boolean;
 }
 
-const QuickQuoteForm = ({initialValues, onSubmit, formSubmitted}: QuickQuoteProps) => {
-  const [showErrorAlert, setShowErrorAlert] = useState(false);
-  const [showSuccessAlert, setShowSuccessAlert] = useState(false);
-  const [isFormValid, setIsFormValid] = useState<boolean | undefined>();
-
-  useEffect(() => {
-    if (formSubmitted && isFormValid) {
-      setShowSuccessAlert(true);
-    }
-
-    if (!formSubmitted && isFormValid === false) {
-      setShowErrorAlert(true);
-    }
-  }, [isFormValid, formSubmitted]);
-
+const QuickQuoteForm = ({initialValues, onSubmit}: QuickQuoteProps) => {
   return (
-    <>
-      <Snackbar
-        color="error"
-        anchorOrigin={{vertical: 'top', horizontal: 'right'}}
-        open={showErrorAlert}
-        onClose={() => setShowErrorAlert(false)}
-        autoHideDuration={5000}
-      >
-        <Alert severity="error">
-          There are one or more errors in the form. Please fix them and try again.
-        </Alert>
-      </Snackbar>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={createQuoteSchema}
+      validateOnChange={false}
+      validateOnBlur={false}
+    >
+      {({errors}: FormikProps<InitialQuoteFormValues>) => {
+        return (
+          <Form>
+            <Grid container spacing={3}>
+              <Grid item xs={12} sm={6}>
+                <Field name="origin">
+                  {({field}: FieldProps) => (
+                    <>
+                      <TextField
+                        {...field}
+                        error={!!errors.origin}
+                        label="From"
+                        variant="outlined"
+                      />
+                      {errors.origin && <FormHelperText error>{errors.origin}</FormHelperText>}
+                    </>
+                  )}
+                </Field>
+              </Grid>
 
-      <Snackbar
-        color="success"
-        anchorOrigin={{vertical: 'top', horizontal: 'right'}}
-        open={showSuccessAlert}
-        onClose={() => setShowSuccessAlert(false)}
-        autoHideDuration={5000}
-      >
-        <Alert severity="success">
-          Your quote was successfully created!
-        </Alert>
-      </Snackbar>
+              <Grid item xs={12} sm={6}>
+                <Field name="destination">
+                  {({field}: FieldProps) => (
+                    <>
+                      <TextField
+                        {...field}
+                        error={!!errors.destination}
+                        label="Destination"
+                        variant="outlined"
+                      />
+                      {errors.destination && <FormHelperText error>{errors.destination}</FormHelperText>}
+                    </>
+                  )}
+                </Field>
+              </Grid>
 
-      <Formik
-        initialValues={initialValues}
-        onSubmit={onSubmit}
-        validationSchema={createQuoteSchema}
-        validateOnChange={false}
-        validateOnBlur={false}
-      >
-        {({errors, isValid}: FormikProps<InitialQuoteFormValues>) => {
-          setIsFormValid(isValid);
+              <Grid item xs={12} sm={6}>
+                <Field name="depart_datetime">
+                  {({field}: FieldProps) => (
+                    <>
+                      <TextField
+                        {...field}
+                        error={!!errors.depart_datetime}
+                        label="Depart Date"
+                        type="datetime-local"
+                        InputLabelProps={{
+                          shrink: true,
+                        }}
+                        variant="outlined"
+                      />
+                      {errors.depart_datetime && <FormHelperText error>{errors.depart_datetime}</FormHelperText>}
+                    </>
+                  )}
+                </Field>
+              </Grid>
 
-          return (
-            <Form>
-              <Grid container spacing={3}>
-                <Grid item xs={12} sm={6}>
-                  <Field name="origin">
+              <Grid item xs={12} sm={6}>
+                <Field name="return_datetime">
+                  {({field}: FieldProps) => (
+                    <>
+                      <TextField
+                        {...field}
+                        error={!!errors.return_datetime}
+                        label="Return Date"
+                        type="datetime-local"
+                        InputLabelProps={{
+                          shrink: true,
+                        }}
+                        variant="outlined"
+                      />
+                      {errors.return_datetime && <FormHelperText error>{errors.return_datetime}</FormHelperText>}
+                    </>
+                  )}
+                </Field>
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+                <Field name="number_people">
+                  {({field}: FieldProps) => (
+                    <FormControl error={!!errors.number_people}>
+                      <InputLabel id="label-people">People</InputLabel>
+                      <Select
+                        {...field}
+                        label="People"
+                        labelId="label-people"
+                        variant="outlined"
+                      >
+                        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num: number) => (
+                          <MenuItem value={num} key={num}>{num}</MenuItem>
+                        ))}
+                      </Select>
+                      {errors.number_people && <FormHelperText error>{errors.number_people}</FormHelperText>}
+                    </FormControl>
+                  )}
+                </Field>
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+                <FormControl error={!!errors.transportation}>
+                  <InputLabel id="label-transportation">Transportation</InputLabel>
+                  <Field name="transportation">
                     {({field}: FieldProps) => (
                       <>
-                        <TextField
-                          {...field}
-                          error={!!errors.origin}
-                          label="From"
-                          variant="outlined"
-                        />
-                        {errors.origin && <FormHelperText error>{errors.origin}</FormHelperText>}
-                      </>
-                    )}
-                  </Field>
-                </Grid>
-
-                <Grid item xs={12} sm={6}>
-                  <Field name="destination">
-                    {({field}: FieldProps) => (
-                      <>
-                        <TextField
-                          {...field}
-                          error={!!errors.destination}
-                          label="Destination"
-                          variant="outlined"
-                        />
-                        {errors.destination && <FormHelperText error>{errors.destination}</FormHelperText>}
-                      </>
-                    )}
-                  </Field>
-                </Grid>
-
-                <Grid item xs={12} sm={6}>
-                  <Field name="depart_datetime">
-                    {({field}: FieldProps) => (
-                      <>
-                        <TextField
-                          {...field}
-                          error={!!errors.depart_datetime}
-                          label="Depart Date"
-                          type="datetime-local"
-                          InputLabelProps={{
-                            shrink: true,
-                          }}
-                          variant="outlined"
-                        />
-                        {errors.depart_datetime && <FormHelperText error>{errors.depart_datetime}</FormHelperText>}
-                      </>
-                    )}
-                  </Field>
-                </Grid>
-
-                <Grid item xs={12} sm={6}>
-                  <Field name="return_datetime">
-                    {({field}: FieldProps) => (
-                      <>
-                        <TextField
-                          {...field}
-                          error={!!errors.return_datetime}
-                          label="Return Date"
-                          type="datetime-local"
-                          InputLabelProps={{
-                            shrink: true,
-                          }}
-                          variant="outlined"
-                        />
-                        {errors.return_datetime && <FormHelperText error>{errors.return_datetime}</FormHelperText>}
-                      </>
-                    )}
-                  </Field>
-                </Grid>
-
-                <Grid item xs={12} sm={6}>
-                  <Field name="number_people">
-                    {({field}: FieldProps) => (
-                      <FormControl error={!!errors.number_people}>
-                        <InputLabel id="label-people">People</InputLabel>
                         <Select
                           {...field}
-                          label="People"
-                          labelId="label-people"
+                          label="Transportation"
+                          labelId="label-transportation"
                           variant="outlined"
                         >
-                          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num: number) => (
-                            <MenuItem value={num} key={num}>{num}</MenuItem>
+                          {Object.values(TransportationType).map((transportationType: string, key: number) => (
+                            <MenuItem value={transportationType} key={key}>{transportationType}</MenuItem>
                           ))}
                         </Select>
-                        {errors.number_people && <FormHelperText error>{errors.number_people}</FormHelperText>}
-                      </FormControl>
-                    )}
-                  </Field>
-                </Grid>
-
-                <Grid item xs={12} sm={6}>
-                  <FormControl error={!!errors.transportation}>
-                    <InputLabel id="label-transportation">Transportation</InputLabel>
-                    <Field name="transportation">
-                      {({field}: FieldProps) => (
-                        <>
-                          <Select
-                            {...field}
-                            label="Transportation"
-                            labelId="label-transportation"
-                            variant="outlined"
-                          >
-                            {Object.values(TransportationType).map((transportationType: string, key: number) => (
-                              <MenuItem value={transportationType} key={key}>{transportationType}</MenuItem>
-                            ))}
-                          </Select>
-                          {errors.transportation && <FormHelperText error>{errors.transportation}</FormHelperText>}
-                        </>
-                      )}
-                    </Field>
-                  </FormControl>
-                </Grid>
-
-                <Grid item xs={12} sm={6}>
-                  <Field name="name">
-                    {({field}: FieldProps) => (
-                      <>
-                        <TextField
-                          {...field}
-                          error={!!errors.name}
-                          label="Name"
-                          variant="outlined"
-                        />
-                        {errors.name && <FormHelperText error>{errors.name}</FormHelperText>}
+                        {errors.transportation && <FormHelperText error>{errors.transportation}</FormHelperText>}
                       </>
                     )}
                   </Field>
-                </Grid>
-
-                <Grid item xs={12} sm={6}>
-                  <Button
-                    className="create-a-quote"
-                    type="submit"
-                    variant="contained"
-                    color="secondary"
-                    disableElevation
-                  >
-                    Create a quote
-                  </Button>
-                </Grid>
+                </FormControl>
               </Grid>
-            </Form>
-          );
-        }}
-      </Formik>
-    </>
+
+              <Grid item xs={12} sm={6}>
+                <Field name="name">
+                  {({field}: FieldProps) => (
+                    <>
+                      <TextField
+                        {...field}
+                        error={!!errors.name}
+                        label="Name"
+                        variant="outlined"
+                      />
+                      {errors.name && <FormHelperText error>{errors.name}</FormHelperText>}
+                    </>
+                  )}
+                </Field>
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+                <Button
+                  className="create-a-quote"
+                  type="submit"
+                  variant="contained"
+                  color="secondary"
+                  disableElevation
+                >
+                  Create a quote
+                </Button>
+              </Grid>
+            </Grid>
+          </Form>
+        );
+      }}
+    </Formik>
   );
 };
 
--- src/validation/createQuoteSchema.ts
+++ src/validation/createQuoteSchema.ts
@@ -2,7 +2,7 @@ import * as Yup from 'yup';
 import moment from 'moment';
 import {InitialQuoteFormValues} from "../utils/interfaces";
 
-export const createQuoteSchema = Yup.object().shape<InitialQuoteFormValues>({
+const createQuoteSchema = Yup.object().shape<InitialQuoteFormValues>({
   destination: Yup.string().required('This field is required'),
   origin: Yup.string().required('This field is required'),
   depart_datetime: Yup.string()
@@ -10,7 +10,7 @@ export const createQuoteSchema = Yup.object().shape<InitialQuoteFormValues>({
     .test('depart_datetime_before_return_datetime',
       'The departure date must be before the return date',
       function (date) {
-        return moment(date).isBefore(moment(this.parent.destination_datetime));
+        return moment(date).isBefore(moment(this.parent.return_datetime));
       },
     ),
   return_datetime: Yup.string()
@@ -18,10 +18,12 @@ export const createQuoteSchema = Yup.object().shape<InitialQuoteFormValues>({
     .test('return_datetime_after_depart_datetime',
       'The return date must be after the departure date',
       function (date) {
-        return moment(date).isAfter(moment(this.parent.origin_datetime));
+        return moment(date).isAfter(moment(this.parent.depart_datetime));
       },
     ),
   number_people: Yup.number().required('This field is required'),
   transportation: Yup.string().required('This field is required'),
   name: Yup.string().required('This field is required'),
 });
+
+export default createQuoteSchema;
